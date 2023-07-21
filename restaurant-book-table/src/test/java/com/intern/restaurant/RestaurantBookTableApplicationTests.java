cea99d2abd484e42e70c7d5ea137641b9a8ccee2
Luong Vinh Loi <luongvinhloi_t65@hus.edu.vn>
2023-07-21 16:20:33 +0700

    Create new web app

---
+++ restaurant-book-table/src/test/java/com/intern/restaurant/RestaurantBookTableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intern.restaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantBookTableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
