965a068d77adcdd70cdde4974b6aec95fdf6d443
tienhung239 <140327462+tienhung239@users.noreply.github.com>
2023-08-07 13:22:28 +0700

    Update BookTableService.java

--- restaurant-book-table/src/main/java/com/intern/restaurant/service/BookTableService.java
+++ restaurant-book-table/src/main/java/com/intern/restaurant/service/BookTableService.java
@@ -1,6 +1,7 @@
 package com.intern.restaurant.service;
 
 import java.util.List;
+import java.time.LocalDateTime;
 
 import com.intern.restaurant.dto.BookTableDTO;
 import com.intern.restaurant.model.BookTable;
@@ -9,4 +10,12 @@ public interface BookTableService {
 	
 	public BookTable createBookTable(BookTable bookTable);
 	public List<BookTableDTO> listBookTable();
+
+	public BookTable getBookTableByBookTableId(Integer btId);
+
+	public List<BookTableDTO> getBookTableByBookTableDate(LocalDateTime dataTime);
+
+	public boolean updateByBookTableId(Integer btId, BookTable updateBookTable);
+
+	public void deleteByBookTableId(int id);
 }
