babcd11a40cb3e3eded4e35338d6ff564b9586ec
tienhung239 <140327462+tienhung239@users.noreply.github.com>
2023-08-07 12:42:12 +0700

    Update BookTableController.java

--- restaurant-book-table/src/main/java/com/intern/restaurant/controller/BookTableController.java
+++ restaurant-book-table/src/main/java/com/intern/restaurant/controller/BookTableController.java
@@ -29,4 +29,27 @@ public class BookTableController {
 	public ResponseEntity<?> listBookTable() {
 		return ResponseEntity.ok(bookTableService.listBookTable());
 	}
+
+	@GetMapping("/by/date")
+	public ResponseEntity<?> getBookTableByDate(@RequestParam("date") String date) {
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+		LocalDateTime ldt = LocalDateTime.parse(date,dtf);
+		return ResponseEntity.ok(bookTableService.getBookTableByBookTableDate(ldt));
+	}
+
+
+	@GetMapping("/by/{id}")
+	public BookTable getBookTableById(@PathVariable("id") int id) {
+		return bookTableService.getBookTableByBookTableId(id);
+	}
+
+	@PutMapping("/update/{id}")
+	public ResponseEntity<?> updateBookTableById(@PathVariable("id") Integer id, @RequestBody BookTable updateBookTable) {
+		return ResponseEntity.ok(bookTableService.updateByBookTableId(id,updateBookTable));
+	}
+
+	@DeleteMapping("/delete/{id}")
+	public void deleteBookTableById(@PathVariable("id") int id) {
+		bookTableService.deleteByBookTableId(id);
+	}
 }
