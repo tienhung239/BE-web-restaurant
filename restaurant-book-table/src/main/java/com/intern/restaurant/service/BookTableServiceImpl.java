b5c26780031d8d95a55ee76bae64eb387cc41f42
tienhung239 <140327462+tienhung239@users.noreply.github.com>
2023-08-07 13:24:18 +0700

    Update BookTableServiceImpl.java

--- restaurant-book-table/src/main/java/com/intern/restaurant/service/BookTableServiceImpl.java
+++ restaurant-book-table/src/main/java/com/intern/restaurant/service/BookTableServiceImpl.java
@@ -3,6 +3,7 @@ package com.intern.restaurant.service;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -42,6 +43,54 @@ public class BookTableServiceImpl implements BookTableService{
 		if (listDto.size() == 0)
 			throw new BookTableException("Book-Table is not found");
 		return listDto;
+	}
+
+		@Override
+	public BookTable getBookTableByBookTableId(Integer btId) throws BookTableException {
+		return bookTableRepository.findById(btId).orElseThrow(()-> new BookTableException("No order exists with given OrderId "+ btId));
+
+
+	}
+
+	@Override
+	public List<BookTableDTO> getBookTableByBookTableDate(LocalDateTime dataTime) {
+		List<BookTableDTO> listDto = new ArrayList<>();
+
+		for (BookTable bookTable: bookTableRepository.findByDatetime(dataTime)) {
+			listDto.add(BookTableMapper.toBookTableDTO(bookTable));
+		}
+		if (listDto.size() == 0)
+			throw new BookTableException("Book-Table-Date is not found");
+		return listDto;
+	}
+
+	@Override
+	public boolean updateByBookTableId(Integer btId, BookTable updateBookTable) {
+
+		Optional<BookTable> optionalBookTable = bookTableRepository.findById(btId);
+		if (optionalBookTable.isPresent()) {
+			if (!bookTableRepository.findById(updateBookTable.getBt_id()).isPresent())
+				return false;
+			else {
+				BookTable category = optionalBookTable.get();
+				category.setNote(updateBookTable.getNote());
+				category.setUser(updateBookTable.getUser());
+				category.setDatetime(updateBookTable.getDatetime());
+				category.setFood(updateBookTable.getFood());
+				category.setNumberPeople(updateBookTable.getNumberPeople());
+				bookTableRepository.save(category);
+				return true;
+			}
+		}
+		return false;
+	}
+
+	@Override
+	public void deleteByBookTableId(int id) {
+		Optional<BookTable> optionalBookTable = bookTableRepository.findById(id);
+		if (!optionalBookTable.isPresent())
+			throw new BookTableException("Book Table not found");
+		bookTableRepository.deleteById(id);
 	}
 
 }
